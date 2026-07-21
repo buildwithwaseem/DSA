b29f49ef6f1e8abea7cf735c9f3e4eea759d12ac
buildwithwaseem <buildwithwasu@gmail.com>
2026-07-21 08:56:43 +0530

    started some basic maths

---
+++ Striver-A2Z-Sheet/Basic-Maths/count_digits.py
@@ -0,0 +1,32 @@
+'''	Count all Digits of a Number '''
+
+#Brute Force Approach
+def countDigits(N):
+    count = 0
+    while N > 0:
+        count += 1
+        N //= 10
+    return count
+if __name__ == "__main__":
+    N = 329823
+    print("N:", N)
+    digits = countDigits(N)
+    print("Number of Digits in N:", digits)
+
+
+
+#Optimal Approach
+
+
+
+import math
+def countDigits(N):
+    return math.floor(math.log10(N)) + 1
+
+
+
+if __name__ == "__main__":
+    N = 329823
+    print("N:", N)
+    digits = countDigits(N)
+    print("Number of Digits in N:", digits)
---
+++ Striver-A2Z-Sheet/Basic-Maths/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+def countDigits(n):
+    count = 0
+    while n > 0:
+        count += 1
+        n //= 10
+    return count
